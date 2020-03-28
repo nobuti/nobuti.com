09de229dc51c4419a86c7942f67a6582d776b2cd
nobuti <buti@nobuti.com>
2020-03-28 20:19:29 +0100

    Gatsby plugin for blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-mdx"],
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        extensions: [".mdx", ".md"],
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: "src/posts",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-feed",
+      options: require("./feed"),
+    },
+  ],
 };
